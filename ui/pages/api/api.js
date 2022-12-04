3f2dfd6dafab3e7234ef5add51f60498d996b052
Wael Ramadan <thebigguyis@gmail.com>
2022-12-04 16:01:23 +0200

    add config

--- ui/pages/api/api.js
+++ ui/pages/api/api.js
@@ -1,5 +1,9 @@
+const config = require('config');
+
+const api_url = config.get('api.url');
+
 export default async function handler(req, res) {
-  const response = await fetch('http://localhost:3001/api/v1/users');
+  const response = await fetch(api_url+'/users');
   //Return the content of the data file in json format
   var data = await response.json()
   res.status(200).send(data);
